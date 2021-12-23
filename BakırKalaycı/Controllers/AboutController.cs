6e7bec3ed8ff260413fadbc90cd76e7303b9a723
VERTAS_OFFICE <VERTAS_OFFICE@DESKTOP-V9SOTVS>
2021-12-23 16:02:29 +0300

    Add project files.

---
+++ BakırKalaycı/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BakırKalaycı.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult Index()
+        {
+            ViewBag.title = "Bakırkalaycı|Hakkımızda";
+            return View();
+        }
+    }
+}
---
+++ BakırKalaycı/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BakırKalaycı.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Title = "BakırKalaycı|2021";
+            return View();
+        }
+        public ActionResult Slider()
+        {
+            return View();
+        }
+        public ActionResult Info()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Project()
+        {
+            return View();
+        }
+        public ActionResult Count()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+
+
+    }
+}
